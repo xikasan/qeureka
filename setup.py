ddc88d3ada9d6b9f0e4dcdc340e64f86e9bd1441
xikasan <xika0907@gmail.com>
2023-12-14 01:09:29 +0900

    create setip.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xqeureka',
+    version='0.0.0',
+    packages=find_packages(),
+    description='Quantum computing applications and scripts for studies',
+    author='xikasan',
+    url='https://github.com/xikasan/xqeureka',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
